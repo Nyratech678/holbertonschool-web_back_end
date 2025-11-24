b4432987dd3d4904e307c11c2059a1070d5207ce
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-11-24 10:09:02 +0100

    Add type-annotated concat function and main script for testing

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes two strings str1 and str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings"""
+    return str1 + str2
