b9793a9f71a6f4bf25738cc870a7c639eb895a68
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-11-24 10:12:59 +0100

    Add type-annotated floor function and main script for testing

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function floor that takes a float n as argument"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    return int(n // 1)
