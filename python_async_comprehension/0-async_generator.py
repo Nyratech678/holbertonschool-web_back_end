ba1ca732b040a275437b4a498125829b13de3e1e
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-11-27 10:20:38 +0100

    Refactor async_generator function to remove type hinting and update docstring

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-""" coroutine called async_generator that takes no arguments"""
+"""Async Generator module"""
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """Yields a random number between 0 and 10 every second, for 10 seconds."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
