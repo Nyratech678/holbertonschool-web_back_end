da2de44bef8e8267af0d4f3154720bbf794eb950
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-11-27 11:08:02 +0100

    Add type hinting to async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator module"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random number between 0 and 10 every second, for 10 seconds."""
 
     for _ in range(10):
