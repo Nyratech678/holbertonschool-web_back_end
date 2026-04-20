2508cf47cbea4e0a5b5ec4c99ff511ebe398f5a2
Nyratech678 <uwuchocolatemilk09876@proton.me>
2026-04-20 09:44:55 +0200

    Fix shebang line and update return type in index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+"""Simple helper function for pagination"""
 
 def index_range(page, page_size):
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return [start_index, end_index]
+    return (start_index, end_index)
