3dbfa4878310cc79a01913f174e190f0b3686eff
Nyratech678 <uwuchocolatemilk09876@proton.me>
2026-04-22 15:38:24 +0200

    create file 12-log_stats to provide statistics about Nginx logs stored in MongoDB

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Script that provides stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Provides stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    # Total number of logs
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    # Methods stats
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    # Status check (GET /status)
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(status_check))
+
+
+if __name__ == "__main__":
+    log_stats()
