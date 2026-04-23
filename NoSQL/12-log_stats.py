20bd8804011e405e17d5bb183afc4ae68131de6c
Nyratech678 <uwuchocolatemilk09876@proton.me>
2026-04-23 10:43:09 +0200

    refactor: streamline log_stats function for improved readability and performance

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Script that provides stats about Nginx logs stored in MongoDB"""
+""" Provide Nginx stats, stored in MongoDB """
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Provides stats about Nginx logs stored in MongoDB"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-
-    # Total number of logs
-    total_logs = nginx_collection.count_documents({})
-    print("{} logs".format(total_logs))
-
-    # Methods stats
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
-
-    # Status check (GET /status)
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print("{} status check".format(status_check))
-
-
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    get_count = collection.count_documents({"method": "GET"})
+    post_count = collection.count_documents({"method": "POST"})
+    put_count = collection.count_documents({"method": "PUT"})
+    patch_count = collection.count_documents({"method": "PATCH"})
+    delete_count = collection.count_documents({"method": "DELETE"})
+
+    status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+
+    print(
+        f"{total_logs} logs\n"
+        "Methods:\n"
+        f"\tmethod GET: {get_count}\n"
+        f"\tmethod POST: {post_count}\n"
+        f"\tmethod PUT: {put_count}\n"
+        f"\tmethod PATCH: {patch_count}\n"
+        f"\tmethod DELETE: {delete_count}\n"
+        f"{status_count} status check"
+    )
