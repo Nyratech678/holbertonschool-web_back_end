dc71212407a855b973be51d0d84bed1e5cb2d49a
Nyratech678 <uwuchocolatemilk09876@proton.me>
2026-05-11 12:55:04 +0200

    fix: correct output handling for stdin data and closing message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
-process.stdin.on('data', (chunk) => {
-  const name = chunk.toString().trim();
-  console.log(`Your name is: ${name}`);
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
-
-console.log('Welcome to Holberton School, what is your name?');
